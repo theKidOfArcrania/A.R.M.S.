5c1c870f9288a8e76d387934e649f3888f0e3585
theKidOfArcrania <wanghenry1998@gmail.com>
2015-09-08 22:14:07 -0500

    Fix merge errors

--- A.R.M.S/src/armsgame/card/WeaponSet.java
+++ A.R.M.S/src/armsgame/card/WeaponSet.java
@@ -205,10 +205,6 @@ public class WeaponSet implements Iterable<PartCard>, Serializable, Observable {
 		return parts.remove(o);
 	}
 
-	public boolean remove(WeaponPart o) {
-		return parts.remove(o);
-	}
-
 	public WeaponSet removeFullSet() {
 		if (!isFullSet()) {
 			throw new IllegalStateException("Not a full set.");
