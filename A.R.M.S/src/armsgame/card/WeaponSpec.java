113c71719e7e4d08cf576e0f29e5e44928eca02d
theKidOfArcrania <wanghenry1998@gmail.com>
2015-09-14 22:27:03 -0500

    Fix weapon-specs error

--- A.R.M.S/src/armsgame/card/WeaponSpec.java
+++ A.R.M.S/src/armsgame/card/WeaponSpec.java
@@ -5,89 +5,96 @@
  */
 package armsgame.card;
 
+import java.util.Arrays;
+
 import static armsgame.card.CardDefaults.getCardDefaults;
 
 /**
  *
  * @author HW
  */
-public class WeaponSpec
-{
-
+public class WeaponSpec {
+	static {
+		String[] specsList = CardDefaults.getCardDefaults()
+			.getProperty("specs.$list")
+			.split("\\s*,\\s*");
+		specs = Arrays.stream(specsList)
+			.parallel()
+			.map(WeaponSpec::new)
+			.toArray(WeaponSpec[]::new);
+	}
 	/*
 	 * Knife("Kfe", 0x6B453A), Pistol("Pis", 0x101050), Mine("Mne", 0x105600), Shotgun("Shg", 0x7C2AB2), Rifle("Rfl", 0xED7423), Sniper("Snp", // 0x86E1EF), Rocket("Rkt", 0xDD0000), Nuke("Nke", 0xFFFF00);
 	 */
 
+	private static final WeaponSpec[] specs;
+
+	public static WeaponSpec identifySpec(String codeType) {
+		for (WeaponSpec spec : specs) {
+			if (spec.getCodeName()
+				.equals(codeType)) {
+				return spec;
+			}
+		}
+		return null;
+	}
+
 	private final String codeType;
 
-	WeaponSpec(String codeType)
-	{
+	private WeaponSpec(String codeType) {
 		this.codeType = codeType;
 	}
 
-	public String getCodeName()
-	{
+	public String getCodeName() {
 		return codeType;
 	}
 
-	public String getEnergyFormName()
-	{
+	public String getEnergyFormName() {
 		return getInternalProperty("energyForm");
 	}
 
-	public String getInternalType()
-	{
+	public String getInternalType() {
 		return "specs." + codeType;
 	}
 
-	public String getLongName()
-	{
+	public String getLongName() {
 		return getInternalProperty("longName");
 	}
 
-	public int getRGBColor()
-	{
+	public int getRGBColor() {
 		return getInternalIntProperty("rgbColor");
 	}
 
-	public String getShortName()
-	{
+	public String getShortName() {
 		return getInternalProperty("shortName");
 	}
 
 	@Override
-	public String toString()
-	{
-		return name() + " weapon-spec";
+	public String toString() {
+		return getShortName() + " weapon-spec";
 	}
 
-	protected double getInternalDoubleProperty(String subKey)
-	{
+	protected double getInternalDoubleProperty(String subKey) {
 		return getCardDefaults().getDoubleProperty(getInternalType() + "." + subKey, 0.0);
 	}
 
-	protected double getInternalDoubleProperty(String subKey, double defValue)
-	{
+	protected double getInternalDoubleProperty(String subKey, double defValue) {
 		return getCardDefaults().getDoubleProperty(getInternalType() + "." + subKey, defValue);
 	}
 
-	protected int getInternalIntProperty(String subKey)
-	{
+	protected int getInternalIntProperty(String subKey) {
 		return getCardDefaults().getIntProperty(getInternalType() + "." + subKey, 0);
 	}
 
-	protected int getInternalIntProperty(String subKey, int defValue)
-	{
+	protected int getInternalIntProperty(String subKey, int defValue) {
 		return getCardDefaults().getIntProperty(getInternalType() + "." + subKey, defValue);
 	}
 
-	protected String getInternalProperty(String subKey)
-	{
+	protected String getInternalProperty(String subKey) {
 		return getCardDefaults().getProperty(getInternalType() + "." + subKey);
 	}
 
-	protected String getInternalProperty(String subKey, String defValue)
-	{
+	protected String getInternalProperty(String subKey, String defValue) {
 		return getCardDefaults().getProperty(getInternalType() + "." + subKey, defValue);
 	}
 }
