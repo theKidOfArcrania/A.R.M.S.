49efb4eeec700db1b58f953d5ba4062500ba171c
theKidOfArcrania <wanghenry1998@gmail.com>
2015-10-02 13:22:36 -0500

    Responses

--- src/armsgame/card/Response.java
+++ src/armsgame/card/Response.java
@@ -1,19 +1,13 @@
 package armsgame.card;
 
-import java.util.function.UnaryOperator;
-
-import armsgame.impl.WeaponTransfer;
-import armsgame.weapon.DamageSpec;
-
 public abstract class Response extends Action {
-	public interface Damage extends UnaryOperator<DamageSpec> {
-	}
-
-	public interface Transfer extends UnaryOperator<WeaponTransfer> {
-	}
 
 	public abstract Damage getDamageResponse();
 
 	public abstract Transfer getTransferResponse();
+
+	public abstract boolean supportsDamageResponse();
+
+	public abstract boolean supportsTransferResponse();
 
 }
--- src/armsgame/weapon/DamageReport.java
+++ src/armsgame/weapon/DamageReport.java
@@ -1,5 +1,37 @@
 package armsgame.weapon;
 
+import armsgame.impl.Player;
+
 public class DamageReport {
-	public DamageReport(Player attacker, Player victim, double
+	private final Player attacker;
+	private final Player victim;
+	private final double dmg;
+	private final boolean vampiric;
+
+	public DamageReport(Player attacker, Player victim, double dmg, boolean vampiric) {
+		this.attacker = attacker;
+		this.victim = victim;
+		this.dmg = dmg;
+		this.vampiric = vampiric;
+	}
+
+	public void execute() {
+
+	}
+
+	public Player getAttacker() {
+		return attacker;
+	}
+
+	public double getDmg() {
+		return dmg;
+	}
+
+	public Player getVictim() {
+		return victim;
+	}
+
+	public boolean isVampiric() {
+		return vampiric;
+	}
 }
